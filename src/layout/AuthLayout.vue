cc9540ac05212aee0aa21e27fb9a15edba91e240
Breimerct98 <breimer.correa@zabud.com.co>
2024-07-10 05:09:08 -0500

    feat: Add AuthLayout component for handling authentication layout

---
+++ src/layout/AuthLayout.vue
@@ -0,0 +1,16 @@
+import { RouterView } from 'vue-router';
+<template>
+  <v-layout>
+    <v-row align="center">
+      <v-col cols="12" align="center">
+        <v-card class="text-left pa-4" variant="tonal" max-width="450">
+          <RouterView />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-layout>
+</template>
+
+<script lang="ts" setup>
+import { RouterView } from 'vue-router';
+</script>
